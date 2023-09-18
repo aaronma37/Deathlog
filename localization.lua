f4b0be2ce215746bef1cb8c799dc3db44ba80694
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2023-09-18 14:19:41 +0900

    added fonts by locale logic

---
+++ localization.lua
@@ -0,0 +1,59 @@
+function getNormalFontPathByLocale(localeCode)
+    if localeCode == "koKR" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "frFR" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "deDE" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "enGB" or localeCode == "enUS" then
+        return "Fonts\\blei00d.TTF"
+    elseif localeCode == "itIT" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "zhCN" then
+        return "Fonts\\ARKai_T.ttf"
+    elseif localeCode == "zhTW" then
+        return "Fonts\\bHEI01B.ttf"
+    elseif localeCode == "ruRU" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "esES" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "esMX" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "ptBR" then
+        return "Fonts\\2002.ttf"
+    else
+        return "Fonts\\blei00d.TTF" -- set default to English font
+    end
+end
+
+function getTitleFontPathByLocale(localeCode)
+    if localeCode == "koKR" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "frFR" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "deDE" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "enGB" or localeCode == "enUS" then
+        return "Fonts\\FRIZQT__.TTF"
+    elseif localeCode == "itIT" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "zhCN" then
+        return "Fonts\\ARKai_T.ttf"
+    elseif localeCode == "zhTW" then
+        return "Fonts\\bHEI01B.ttf"
+    elseif localeCode == "ruRU" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "esES" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "esMX" then
+        return "Fonts\\2002.ttf"
+    elseif localeCode == "ptBR" then
+        return "Fonts\\2002.ttf"
+    else
+        return "Fonts\\FRIZQT__.TTF" -- set default to English font
+    end
+end
+
+_localeCode = GetLocale()
+normalFont = getNormalFontPathByLocale(_localeCode)
+titleFont = getTitleFontPathByLocale(_localeCode)
