8d47e13d26a4c4ead21206dcfff6c852058df6ce
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2023-09-22 21:06:47 +0900

    add strings localization logic

---
+++ Localization/localization.lua
@@ -0,0 +1,35 @@
+local localeMap = {
+    ["koKR"] = strings_ko,
+    ["enGB"] = strings_en,
+    ["enUS"] = strings_en,
+    ["deDE"] = strings_de,
+    ["frFR"] = strings_fr,
+    ["itIT"] = strings_it,
+    ["zhCN"] = strings_cn,
+    ["zhTW"] = strings_cn,
+    ["ruRU"] = strings_ru,
+    ["esES"] = strings_es,
+    ["esMX"] = strings_es,
+    ["ptBR"] = strings_pt,
+    ["default"] = strings_en -- Default string to English
+}
+
+local function loadLocaleData(localeCode)
+    local localeData = localeMap[localeCode] or localeMap["default"]
+
+    -- Set the fallback mechanism in case the table is incomplete
+    setmetatable(localeData, {
+        __index = function(table, key)
+            return localeMap["default"][key]
+        end
+    })
+
+    return localeData
+end
+
+local localeCode = GetLocale()
+
+-- set global variables for Localization table
+L = loadLocaleData(localeCode)
+-- Use it like
+-- SetFont(L.mainFont)
