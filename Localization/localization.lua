56058c0759a41b011cbf29edf2c4d03d0bbfedb2
aaronma37 <aam021@eng.ucsd.edu>
2023-09-24 11:40:56 -0700

    Revert "remove default key mechanism from localization"
    This reverts commit c8c7b097c5746c7dc05afcae2c66fcee39060d4b.

--- Localization/localization.lua
+++ Localization/localization.lua
@@ -1,22 +1,30 @@
 local localeMap = {
-	["koKR"] = strings_ko,
-	["enGB"] = strings_en,
-	["enUS"] = strings_en,
-	["deDE"] = strings_de,
-	["frFR"] = strings_fr,
-	["itIT"] = strings_it,
-	["zhCN"] = strings_cn,
-	["zhTW"] = strings_cn,
-	["ruRU"] = strings_ru,
-	["esES"] = strings_es,
-	["esMX"] = strings_es,
-	["ptBR"] = strings_pt,
-	["default"] = strings_en, -- Default string to English
+    ["koKR"] = strings_ko,
+    ["enGB"] = strings_en,
+    ["enUS"] = strings_en,
+    ["deDE"] = strings_de,
+    ["frFR"] = strings_fr,
+    ["itIT"] = strings_it,
+    ["zhCN"] = strings_cn,
+    ["zhTW"] = strings_cn,
+    ["ruRU"] = strings_ru,
+    ["esES"] = strings_es,
+    ["esMX"] = strings_es,
+    ["ptBR"] = strings_pt,
+    ["default"] = strings_en -- Default string to English
 }
 
 local function loadLocaleData(localeCode)
-	local localeData = localeMap[localeCode] or localeMap["default"]
-	return localeData
+    local localeData = localeMap[localeCode] or localeMap["default"]
+
+    -- Set the fallback mechanism in case the table is incomplete
+    setmetatable(localeData, {
+        __index = function(table, key)
+            return localeMap["default"][key]
+        end
+    })
+
+    return localeData
 end
 
 local localeCode = GetLocale()
